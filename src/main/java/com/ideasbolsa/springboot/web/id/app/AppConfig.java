9b4fbe707726f22049e7384d4fd0144dfa4c65f5
César Vargas Escorcia <thecesarvargas@gmail.com>
2020-01-13 21:03:52 -0600

    Práctica Factura

--- src/main/java/com/ideasbolsa/springboot/web/id/app/AppConfig.java
+++ src/main/java/com/ideasbolsa/springboot/web/id/app/AppConfig.java
@@ -1,9 +1,14 @@
 package com.ideasbolsa.springboot.web.id.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import com.ideasbolsa.springboot.web.id.app.models.domain.ItemFactura;
+import com.ideasbolsa.springboot.web.id.app.models.domain.Producto;
 import com.ideasbolsa.springboot.web.id.app.models.service.InterfazServicio;
 import com.ideasbolsa.springboot.web.id.app.models.service.MiServicio;
 import com.ideasbolsa.springboot.web.id.app.models.service.MiServicioComplejo;
@@ -29,4 +34,29 @@ public class AppConfig {
 	public InterfazServicio registrarMiServicioComplejo() {
 		return new MiServicioComplejo();
 	}
+	
+
+	@Bean("itemsFactura")
+	public List<ItemFactura> registrarItems(){
+		Producto producto1 = new Producto("Camara Sony", 100);
+		Producto producto2 = new Producto("Bicicleta Biachi", 200);
+		
+		ItemFactura linea1 = new ItemFactura(producto1, 2);
+		ItemFactura linea2 = new ItemFactura(producto2, 4);
+		return Arrays.asList(linea1, linea2);
+		}
+
+	@Bean("itemsFacturaOficina")
+	public List<ItemFactura> registrarOficina(){
+		Producto producto1 = new Producto("Monitor LG LCD ", 250);
+		Producto producto2 = new Producto("Notebook ASUS", 500);
+		Producto producto3 = new Producto("Impresora HP Multifuncional", 80);
+		Producto producto4 = new Producto("Escritorio oficina", 500);
+		
+		ItemFactura linea1 = new ItemFactura(producto1, 2);
+		ItemFactura linea2 = new ItemFactura(producto2, 1);
+		ItemFactura linea3 = new ItemFactura(producto3, 1);
+		ItemFactura linea4 = new ItemFactura(producto4, 1);
+		return Arrays.asList(linea1, linea2, linea3, linea4);
+		}
 }
--- src/main/java/com/ideasbolsa/springboot/web/id/app/controllers/IndexController.java
+++ src/main/java/com/ideasbolsa/springboot/web/id/app/controllers/IndexController.java
@@ -1,20 +1,17 @@
 package com.ideasbolsa.springboot.web.id.app.controllers;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 import com.ideasbolsa.springboot.web.id.app.models.service.InterfazServicio;
 
 
-
-/**
- * @Qualifier permite inyectar según la necesidad, se puede hacer uso de la anotación
- * con osin usar la anotación @Primary
- *
- */
 @Controller
 public class IndexController {
 	
@@ -29,6 +26,6 @@ public class IndexController {
 		model.addAttribute("objeto", servicio.operacion());
 		return "index";
 	}
-
-
+	
+	
 }
